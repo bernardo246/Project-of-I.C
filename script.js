ce932343f25ea3fcea477ab638ce3c9eec23a372
Bernardo Leão <bernardoleao23@gmail.com>
2025-05-29 17:21:02 -0300

    add files of the project

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("botao").addEventListener("click", function () {
+  window.location.href = "https://www.primecursos.com.br/arduino-basico/"; // Altere para o link desejado
+});
+document.getElementById("click").addEventListener("click", function () {
+  window.location.href ="https://youtube.com/playlist?list=PLx4x_zx8csUgWBTvA-fluHV970SzDJRBw&si=vy7s-uHNQQ4KgMe5"; // Altere para o link desejado
+});
+document.getElementById("curiosidades").addEventListener("click", function () {
+  window.location.href = "curiosidades.html"; // Altere para o link desejado
+});
+document.getElementById("cc").addEventListener("click", function () {
+  window.location.href = "cc.html" // Altere para o link desejado
+});
